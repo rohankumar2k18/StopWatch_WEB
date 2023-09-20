51b9cc8c6511f5e33f2518a8230559ae93da7cf4
Rohan kumar <95177864+rohankumar2k18@users.noreply.github.com>
2023-09-21 02:03:10 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -12,12 +12,13 @@ let intervalId;
 let hrs = 0;
 let mins = 0;
 let secs = 0;
+let ms = 0;
 
 startButton.addEventListener("click", () => {
     if (paused) {
         paused = false;
         startTime = Date.now() - elapsedTime;
-        intervalId = setInterval(updateTime, 75);
+        intervalId = setInterval(updateTime, 1);
     }
 });
 pauseButton.addEventListener("click", () => {
@@ -27,7 +28,7 @@ pauseButton.addEventListener("click", () => {
 
 });
 resetButton.addEventListener("click", () => {
-    timeDisplay.textContent = "00 : 00 : 00";
+    timeDisplay.textContent = "00 : 00 : 00 : 000";
     elapsedTime = 0;
     paused = true;
     clearInterval(intervalId);
@@ -36,19 +37,31 @@ resetButton.addEventListener("click", () => {
 function updateTime() {
     elapsedTime = Date.now() - startTime;
 
+    ms = elapsedTime % 1000;
     secs = Math.floor((elapsedTime / 1000) % 60);
     mins = Math.floor((elapsedTime / (1000 * 60)) % 60);
     hrs = Math.floor((elapsedTime / (1000 * 60 * 60)) % 60);
     secs = pads(secs);
     mins = pads(mins);
     hrs = pads(hrs);
+    ms = mpads(ms);
 
-    timeDisplay.textContent = `${hrs} : ${mins} : ${secs}`;
+    timeDisplay.textContent = `${hrs} : ${mins} : ${secs} : ${ms}`;
 
     
 
     function pads(unit){
         return (("0") + unit).length > 2 ? unit : "0" + unit;
+    }
+    function mpads(unit){
+        unit = "" + unit;
+        if(unit.length === 1){
+            unit = "00" + unit;
+        }
+        else if(unit.length === 2){
+            unit = "0" + unit;
+        }
+        return unit;
     }
 }
 
